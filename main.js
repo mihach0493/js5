78a854f6da43d300975d036c0cbef391fb5a4d4f
Mykhailo Chui <mihach0493@gmail>
2020-09-12 20:18:20 +0200

    js5

---
+++ main.js
@@ -0,0 +1,11 @@
+let number = 12;
+
+if (number ===10) {
+    console.log('Podana liczba jest większa od ==10');
+}
+else if (number ===12) {
+    console.log('Podana liczba jest większa od ==12');
+}
+else {
+    console.log('Podana liczba nie jest większa od');
+}
